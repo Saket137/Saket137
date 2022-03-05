a95135200b296060c32a5f79892d77f77ee83466
Saket137 <87948386+Saket137@users.noreply.github.com>
2022-03-05 19:36:12 +0530

    Qualification Code
    Qualification Code

---
+++ index.php
@@ -0,0 +1,72 @@
+
+<html>  
+<head></head>  
+<title>Static Dropdown List</title>  
+<body bgcolor="pink">  
+
+<div id="menubar">
+    <ul>
+   <a href="file:///C:/Users/saket/OneDrive/Desktop/Saket%20Project/jobs.html#"><li>Home</li></a>
+    </ul>
+</div>
+
+<br>
+10th Percentage :  
+<select>  
+  <option value="Select">60% to 80%</option>}  
+  <option value="Vineet">81% to 100%</option>  
+</select></n>
+
+
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+12th Percentage :  
+<select>  
+  <option value="Select">60% to 80%</option>}  
+  <option value="Vineet">81% to 100%</option>  
+</select>
+
+<br>
+<br>
+<br>
+
+
+
+B.Tech Percentage :  
+<select>  
+  <option value="Select">60% to 80%</option>}  
+  <option value="Vineet">81% to 100%</option>  
+</select>   
+
+&nbsp; &nbsp;
+
+B.Sc Percentage :  
+<select>  
+  <option value="Select">60% to 80%</option>}  
+  <option value="Vineet">81% to 100%</option>  
+</select>
+
+&nbsp; &nbsp;
+
+B.Pharma Percentage :  
+<select>  
+  <option value="Select">60% to 80%</option>}  
+  <option value="Vineet">81% to 100%</option>  
+</select>
+
+&nbsp; &nbsp;
+
+B.A. Percentage :  
+<select>  
+  <option value="Select">60% to 80%</option>}  
+  <option value="Vineet">81% to 100%</option>  
+</select>   
+
+<br>
+<br>
+<br>
+<center>
+<button type="submit" style="height: 35px; width: 100px"> <a href="http://localhost/Jobs%20List/Jobs%20List.php">Submit</a></button>
+</center>
+</body>  
+<html>  
