b5542266d257da801754ab36cbd47fbdb9aa733d
Saket137 <87948386+Saket137@users.noreply.github.com>
2022-03-05 19:37:04 +0530

    Jobs List
    Jobs List

---
+++ Jobs List.php
@@ -0,0 +1,56 @@
+<html>  
+<head></head>  
+<title>Static Dropdown List</title>  
+<body bgcolor="brown">
+<h1 style="color:black;font-size:40px;">
+
+    On the basis of your Score. There are some best Jobs for you.</h1>
+    <br>
+    <br>
+    <h2 style="color:white;font: size 20px;">
+    JIPMER Group B and C Various Post Online Form 2022
+    &nbsp;
+    <input type="submit" value=">>Click Here To Know More<<">
+    <br>
+    <br>
+Join Indian Army NCC 52 Entry Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+Indian Army SSC Tech 59 Men & 30 Women Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+Bank Note Press BNP Junior Technician Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+SGPGI Sister Grade II and Other Post Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+JSSC Excise Constable Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+Indian Navy Various Officers Entry Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+Delhi High Court HJS Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+<br>
+<br>
+Delhi High Court Judicial Service Exam Online Form 2022
+&nbsp;
+<input type="submit" value=">>Click Here To Know More<<">
+    </h2>
+</text>
+</body></html>
